367bb257b217f3c2093352feb5fb3e699d5f1965
repeckadonatas <repeckadonatas@yahoo.comm>
2024-05-29 20:54:39 +0300

    Adjusting the logic of api data download function

--- src/get_jobs/get_jobs.py
+++ src/get_jobs/get_jobs.py
@@ -1,15 +1,14 @@
 """
-Functions to get API response and save it as 
-JSON file.
-download_metals_data() function is controlling the
-logic for data download and is used in
-concurrency.
+The main logic of retrieving API responses
+and saving them as JSON files.
+download_api_data() function is controlling the
+logic for data download and is used in concurrency.
 """
 
 import json
 
 import requests
-from requests.exceptions import RequestException
+from requests.exceptions import RequestException, URLRequired, InvalidURL
 
 import src.logger as log
 from src.constants import *
@@ -17,47 +16,35 @@ from src.constants import *
 api_logger = log.app_logger(__name__)
 
 
-def get_metal_price_data(metals: list, 
-                         API_KEY: str):
+def get_jobs_api_data(api_dict: dict) -> json:
     """
-    Get price data for a given precious metal.
-    A JSON file is created containing price data 
-    for every precious metal as it's name is being provided.
-    :param metals: a list with name values of a precious metal
-    :param API_KEY: API key for price API service used
+    Get API response data and save it as JSON files.
+    The function uses a dictionary to retrieve an API URL.
+    :param api_dict: a dictionary of API URLs
     """
-    for metal in metals:
-        api_url = (f'https://api.metals.dev/v1/metal/spot'
-                   f'?api_key={API_KEY}'
-                   f'&metal={metal}'
-                    '&currency=USD')
-
-        headers = {'accept': 'application/json; charset=utf-8'}
-        response = requests.get(api_url, headers=headers)
-        json_response = response.json()
-
-        if json_response["status"] != "success":
-            api_logger.info(f'An error occurred: {json_response["status"]}')
+    for api_name, api_url in api_dict.items():
+        if not api_url:
+            api_logger.info(f'Missing URL for "{api_name}"!\n')
         else:
-            with open(PATH_TO_DATA_STORAGE / (metal + '_response.json'), 'w', encoding='utf-8') as f:
-                json.dump(json_response, f, ensure_ascii=False, indent=4)
-                api_logger.info('Downloading price API data for "{}..."'.format(metal))
+            headers = {'accept': 'application/json; charset=utf-8'}
+            response = requests.get(api_url, headers=headers)
+            json_response = response.json()
+
+            if response.status_code == 200:
+                os.makedirs(PATH_TO_DATA_STORAGE, exist_ok=True)
+                with open(PATH_TO_DATA_STORAGE / (api_name + '_response.json'), 'w', encoding='utf-8') as f:
+                    json.dump(json_response, f, ensure_ascii=False, indent=4)
+                    api_logger.info('Downloading API data for "{}..."'.format(api_name))
+            else:
+                api_logger.info(f'An error occurred: {response.status_code} - {response.text}\n')
 
 
-def download_metals_data():
+def download_api_data():
     """
-    A function to download the price data for
-    every precious metal provided.
-    Gets API key from a text file.
-    Reads a list of metals from a text file.
-    A JSON file is saved for every request.
+
     """
     try:
-        api_key = read_api()
-        metals = read_metals_list()
-        
-        get_metal_price_data(metals, api_key)
-        api_logger.info('Data downloaded successfully!\n')
+        get_jobs_api_data(API_DICT)
     
-    except (Exception, RequestException) as e:
-        api_logger.info('An exception occured: {}'.format(e))
+    except (Exception, RequestException, URLRequired, InvalidURL) as e:
+        api_logger.info('An exception occurred: %s', e, exc_info=True)

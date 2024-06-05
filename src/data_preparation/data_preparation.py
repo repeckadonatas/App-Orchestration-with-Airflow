7c1b87ccc95c9f5337254d59174f9938a7e21b93
repeckadonatas <repeckadonatas@yahoo.comm>
2024-06-05 22:08:10 +0300

    Refining and updating 'data_preparation.py' functions

--- src/data_preparation/data_preparation.py
+++ src/data_preparation/data_preparation.py
@@ -4,30 +4,14 @@ an API responses before uploading the data
 to the database.
 """
 
-import os
 import json
 import pandas as pd
+from pandas.core import series
 
-import source.logger as log
-from source.constants import *
+import src.logger as log
+from src.constants import *
 
 data_logger = log.app_logger(__name__)
-
-
-def get_files_in_directory() -> list:
-    """
-    Reads JSON files in a set directory.
-    Returns a list of names of files in the directory
-    to be iterated through.
-    :return: a list of file names in the directory
-    """
-    files = os.scandir(PATH_TO_DATA_STORAGE)
-
-    list_of_files = []
-    for file in files:
-        if file.is_dir() or file.is_file():
-            list_of_files.append(file.name)
-    return list_of_files
 
 
 def create_dataframe(file_json: str) -> pd.DataFrame:
@@ -35,27 +19,78 @@ def create_dataframe(file_json: str) -> pd.DataFrame:
     Creates a pandas dataframe from a JSON file.
     Requires a name of the file.
     """
-    with open(PATH_TO_DATA_STORAGE / file_json) as jfile:
-        json_data = json.load(jfile)
+    with open(PATH_TO_DATA_STORAGE / file_json, 'r', encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
         df = pd.DataFrame(pd.json_normalize(json_data))
     return df
 
 
-def change_column_names(dataframe: pd.DataFrame) -> pd.DataFrame:
+def flatten_json_file(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Flattens the supplied dataframe and returns a new dataframe.
+    :param dataframe: dataframe to flatten
+    :return: new dataframe with flattened json data
+    """
+    for column in dataframe.columns:
+        if isinstance(dataframe[column], (dict, pd.core.series.Series)):
+            dataframe_flat = dataframe[column].apply(pd.Series)
+            dataframe_flat = dataframe_flat[0].apply(pd.Series)
+
+            for col in dataframe_flat.columns:
+                if isinstance(dataframe_flat[col][0], list):
+                    list_df = dataframe_flat[col].apply(lambda x: pd.Series(x))
+                    list_df = list_df.add_prefix(f"{col}_")
+                    dataframe_flat = dataframe_flat.drop(col, axis=1).join(list_df)
+                    dataframe_flat.fillna('')
+
+                else:
+                    continue
+
+        else:
+            dataframe_flat = dataframe[column].apply(pd.Series)
+            dataframe_flat = dataframe_flat[0].apply(pd.Series)
+
+    return dataframe_flat
+
+
+def rename_columns(dataframe: pd.DataFrame,
+                   column_rename_mapping: dict) -> pd.DataFrame:
     """
     Changes the column names of the dataframe to their new column names.
-    :param dataframe: a pandas dataframe to change column names
+    :param dataframe: a pandas dataframe to change column names for
     :return: dataframe with new column names
-    """
-    new_names = {"rate.price": "rate_price",
-                 "rate.ask": "rate_ask",
-                 "rate.bid": "rate_bid", 
-                 "rate.high": "rate_high",
-                 "rate.low": "rate_low",
-                 "rate.change": "rate_change",
-                 "rate.change_percent": "rate_change_percent"}
-    dataframe.rename(columns=new_names, inplace=True)
+    """    
+    dataframe.rename(columns=column_rename_mapping, inplace=True)
+    dataframe.reindex(columns=column_rename_mapping)
     return dataframe
+
+
+def reorder_dataframe_columns(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Reorders the columns of a dataframe to match a common schema.
+    :param dataframe: dataframe to reorder columns for
+    :return: dataframe with reordered columns
+    """
+    dataframe = dataframe.reindex(columns=COMMON_SCHEMA)
+    return dataframe
+
+
+# def change_column_names(dataframe: pd.DataFrame) -> pd.DataFrame:
+#     """
+#     Changes the column names of the dataframe to their new column names.
+#     :param dataframe: a pandas dataframe to change column names
+#     :return: dataframe with new column names
+#     """
+#     new_names = {"rate.price": "rate_price",
+#                  "rate.ask": "rate_ask",
+#                  "rate.bid": "rate_bid", 
+#                  "rate.high": "rate_high",
+#                  "rate.low": "rate_low",
+#                  "rate.change": "rate_change",
+#                  "rate.change_percent": "rate_change_percent"}
+#     dataframe.rename(columns=new_names, inplace=True)
+#     dataframe.reindex(columns=new_names)
+#     return dataframe
 
 
 def prepare_json_data(queue: str, event: str) -> None:
@@ -67,18 +102,18 @@ def prepare_json_data(queue: str, event: str) -> None:
     """
     while not event.is_set():
         try:
-            json_files = get_files_in_directory()
-            data_logger.info('Files found in a directory: {}'.format(json_files))
+            json_files = get_files_in_directory(PATH_TO_DATA_STORAGE)
+            data_logger.info(f'Files found in a directory: {json_files}')
             
             for json_file in json_files:
                 json_to_df = create_dataframe(json_file)
-                new_col_names = change_column_names(json_to_df)
-                data_logger.info('A dataframe was created for a file: {}'.format(json_file))
+                # new_col_names = change_column_names(json_to_df)
+                data_logger.info(f'A dataframe was created for a file: {json_file}')
 
                 queue.put([new_col_names, json_file])
 
             event.set()
             print()
         except Exception as e:
-            data_logger.error("An error occurred while creating a dataframe: {}\n".format(e))
+            data_logger.error(f"An error occurred while creating a dataframe:\n {e}\n")
         

3535b61db708c29fc362ae0522d44d4782753a60
repeckadonatas <repeckadonatas@yahoo.comm>
2024-06-05 22:07:08 +0300

    Updated 'constants.py' file

--- src/constants.py
+++ src/constants.py
@@ -9,10 +9,8 @@ from datetime import datetime
 
 from dotenv import load_dotenv, find_dotenv
 
-
 # DATE
 CURRENT_DATE = datetime.today().strftime("%Y%m%d_%H%m")
-
 
 # FOR LOGGER ONLY
 LOG_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -21,15 +19,12 @@ FORMATTER = logging.Formatter(f'{LOG_TIME} :: %(name)s :: %(levelname)s :: %(fun
 PATH_TO_LOGS = Path(__file__).cwd()
 LOG_FILE = PATH_TO_LOGS / 'logs/' / ("app_logger_" + datetime.today().strftime("%Y%m%d") + ".log")
 
-
 # DATABASE INITIALIZATION
 INIT_DB = Path(__file__).cwd() / 'sql/init.sql'
-
 
 # FOR PG_DUMP FUNCTION
 # PG_DUMP_PATH = r'C:\Program Files\PostgreSQL\16\bin\pg_dump.exe'
 PG_DUMP_PATH = 'pg_dump'
-
 
 # API URLs
 REMOTIVE_API = "https://remotive.com/api/remote-jobs?limit=1"
@@ -41,34 +36,52 @@ API_DICT = {'REMOTIVE': REMOTIVE_API,
             'JOBICY': JOBICY_API
             }
 
-
 # PATHS TO DATA AND FILES
 PATH_TO_DATA_STORAGE = Path(__file__).cwd() / 'src/data'
-
 
 # BACKUPS LOCATION
 PATH_TO_BACKUPS = Path(__file__).cwd() / 'backups'
 BACKUP_FOLDERS_TODAY = PATH_TO_BACKUPS / ("backup_" + CURRENT_DATE)
 
-
 # BACKUP FOLDERS FOR DATABASE AND ML MODELS
 DB_BACKUP_FILE = BACKUP_FOLDERS_TODAY / ("db_backup_" + CURRENT_DATE + ".sql")
-ML_MODELS_BACKUP_FOLDER = BACKUP_FOLDERS_TODAY / ('ml_models_backup_' + CURRENT_DATE)
 
+# FOR DATAFRAME
+COLUMN_RENAME_MAPPING = {"title": "job_title",
+                         "companyName": "company_name",
+                         "applicationLink": "job_ad_link",
+                         "locationRestrictions": "region",
+                         "minSalary": "minimum_salary",
+                         "maxSalary": "maximum_salary",
+                         "pubDate": "pub_date_timestamp",
+                         "expiryDate": "expiry_date_timestamp",
+
+                         "jobTitle": "job_title",
+                         "url": "job_ad_link",
+                         "jobType": "job_type",
+                         "jobGeo": "region",
+                         "annualSalaryMin": "minimum_salary",
+                         "annualSalaryMax": "maximum_salary",
+
+                         "candidate_required_location": "region",
+                         "publication_date": "pub_date_timestamp"}
 
 # TABLES FOR DB
-# TABLES_TO_CREATE = ['gold_historic',
-#                     'silver_historic',
-#                     'platinum_historic',
-#                     'palladium_historic',
-#                     'commodities_price_data_analytics']
+COMMON_SCHEMA = ['job_title',
+                 'company_name',
+                 'job_ad_link',
+                 'job_type',
+                 'region',
+                 'salary',
+                 'timestamp']
 
-# TABLE_MAPPING = {'gold': 'gold_historic',
-#                  'silver': 'silver_historic',
-#                  'platinum': 'platinum_historic',
-#                  'palladium': 'palladium_historic'}
-#
-# TRAINING_DATA_COLUMNS = ['rate_price', 'rate_ask']
+TABLES_TO_CREATE = ['remotive_data',
+                    'himalayas_data',
+                    'jobicy_data']
+
+TABLE_MAPPING = {'remotive': 'remotive_data',
+                 'himalayas': 'himalayas_data',
+                 'jobicy': 'jobicy_data'}
 
 
 # REUSABLE FUNCTIONS
@@ -106,7 +119,6 @@ def get_files_in_directory(dir_path: str) -> list:
         if file.is_dir() or file.is_file():
             list_of_files.append(file.name)
     return list_of_files
-
 
 # def init_db():
 #     """

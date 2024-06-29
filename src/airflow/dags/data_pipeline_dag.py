1d49c9cbd94f2ec64ede9751cace65abf28cb041
repeckadonatas <repeckadonatas@yahoo.comm>
2024-06-29 22:03:39 +0300

    Adding a DAG file to control scheduled tasks

--- src/airflow/dags/data_pipeline_dag.py
+++ src/airflow/dags/data_pipeline_dag.py
@@ -1,12 +1,23 @@
-from functools import partial
+"""
+Airflow setup to control the tasks of the app.
+"""
+
+import threading
+from queue import Queue
 from datetime import datetime, timedelta
 
 from airflow.models.dag import DAG
 from airflow.operators.python_operator import PythonOperator
 
-from src.get_api_data import get_api_data
-from src.data_preparation import *
-from src.constants import *
+import src.get_api_data as api
+import src.data_preparation as prep
+import src.db_functions.data_upload_sequence as upload
+import src.backup_functions as bckp
+import src.logger as log
+
+from src.constants import DATA_PIPELINE_DAG_SHD, DATABASE_BACKUP_DAG_SHD
+
+dag_logger = log.app_logger(__name__)
 
 default_args = {
     "depends_on_past": False,
@@ -18,34 +29,80 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
+with DAG(
+    "data_pipeline_dag",
+    default_args=default_args,
+    description='Data Pipeline DAG. Schedules data download, transformation and upload to a database.',
+    schedule_interval=DATA_PIPELINE_DAG_SHD,
+    catchup=False,
+) as data_pipeline_dag:
+
+    # Tasks to be run
+    def download_api_data():
+        try:
+            api.download_api_data()
+        except Exception as e:
+            dag_logger.error('Error occurred while downloading API data: %s\n', e, exc_info=True)
+
+    def prepare_json_data():
+        try:
+            event = threading.Event()
+            queue = Queue(maxsize=4)
+            prep.prepare_json_data(queue, event)
+        except Exception as e:
+            dag_logger.error('Error occurred while transforming JSON data: %s\n', e, exc_info=True)
+
+    def jobs_data_upload_to_db():
+        try:
+            event = threading.Event()
+            queue = Queue(maxsize=4)
+            upload.jobs_data_upload_to_db(queue, event)
+        except Exception as e:
+            dag_logger.error('Error occurred while uploading data to a database: %s\n', e, exc_info=True)
+
+
+    # Setting tasks for Airflow
+    task1 = PythonOperator(
+        task_id='download_api_data',
+        python_callable=download_api_data,
+        dag=data_pipeline_dag,
+    )
+
+    task2 = PythonOperator(
+        task_id='prepare_json_data',
+        python_callable=prepare_json_data,
+        dag=data_pipeline_dag,
+    )
+
+    task3 = PythonOperator(
+        task_id='jobs_data_upload_to_db',
+        python_callable=jobs_data_upload_to_db,
+        dag=data_pipeline_dag,
+    )
+
+    # Task dependencies
+    task1 >> task2 >> task3
+
 
 with DAG(
-    'get_api_data',
+    "database_backup_dag",
     default_args=default_args,
-    description='Get data from multiple APIs',
-    schedule_interval='@daily',
+    description='Database backup DAG. Controls the backups schedule.',
+    schedule_interval=DATABASE_BACKUP_DAG_SHD,
     catchup=False,
-) as dag:
+) as backup_dag:
 
-    for api_name, api_url in API_DICT.items():
-        task1_get_api_data = PythonOperator(
-            task_id=f'get_{api_name}_data',
-            python_callable=partial(get_api_data, api_name, api_url),
-            dag=dag
-        )
+    def database_backup():
+        try:
+            bckp.database_backup()
+        except Exception as e:
+            dag_logger.error('Error occurred while backing up a database: %s\n', e, exc_info=True)
 
 
-with DAG(
-    'prepare_json_data',
-    default_args=default_args,
-    description='DAG to prepare JSON data for upload to a database',
-    schedule_interval='@daily',
-    catchup=False,
-) as dag:
+    task4 = PythonOperator(
+        task_id='database_backup',
+        python_callable=database_backup,
+        dag=backup_dag,
+    )
 
-    for json_file in get_files_in_directory(PATH_TO_DATA_STORAGE):
-        task2_prepare_json_data = PythonOperator(
-            task_id=f'preparing_"{json_file}"_data',
-            python_callable=partial(create_dataframe, json_file, COLS_NORMALIZE),
-            dag=dag
-        )
+    task4

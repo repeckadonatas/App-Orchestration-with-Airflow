5a9cee411461ffc8ffb8ad892f5d7ab0a6b14926
repeckadonatas <repeckadonatas@yahoo.comm>
2024-07-17 12:36:10 +0300

    Updated shell script

--- scripts/run_main_services.sh
+++ scripts/run_main_services.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd /mnt/c/Users/HP/Desktop/TURING/Projects/'Module 2'/DE-Capstone-2
+cd /mnt/d/TURING/PROJECTS/MODULE_3/Job-Application-System
 
-echo "Running cron-job of main services 'project-db' and 'project-app'"
+echo "Running cron-job of main service 'project-app'"
 
-docker compose restart project-db project-app
+docker compose restart project-app

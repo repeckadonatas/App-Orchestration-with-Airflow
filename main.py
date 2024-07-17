cc03f6fc1f46132f70ce1f63330c58c10e6cf0ad
repeckadonatas <repeckadonatas@yahoo.comm>
2024-07-17 12:08:04 +0300

    Modified main.py file to take command line argument for API URL from Airflow DAGs

--- main.py
+++ main.py
@@ -1,42 +1,61 @@
 """
 Main file to run the program using
 Python's concurrent.futures module.
-The programs performance is also timed
-and printed out.
 """
 
-import threading
+import sys
+from threading import Event
+from queue import Queue
 import concurrent.futures
 from concurrent.futures import CancelledError, TimeoutError, BrokenExecutor, InvalidStateError
-from queue import Queue
 
 import src.get_api_data as api
 import src.data_preparation as prep
 import src.db_functions.data_upload_sequence as upload
 import src.logger as log
+from src.constants import read_dict, API_DICT
 
 main_logger = log.app_logger(__name__)
 
-event = threading.Event()
-queue = Queue(maxsize=4)
-try:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        # api_data = executor.submit(api.download_api_data())
-        # json_prep = executor.submit(prep.prepare_json_data(queue, event))
-        # db_con = executor.submit(upload.jobs_data_upload_to_db(queue, event))
 
-        tasks = [executor.submit(api.download_api_data()),
-                 executor.submit(prep.prepare_json_data(queue, event)),
-                 executor.submit(upload.jobs_data_upload_to_db(queue, event))]
+if __name__ == '__main__':
 
-    concurrent.futures.wait(tasks)
-except CancelledError as e:
-    main_logger.error('CancelledError occurred while running "main.py": %s\n', e, exc_info=True)
-except TimeoutError as e:
-    main_logger.error('TimeoutError occurred while running "main.py": %s\n', e, exc_info=True)
-except BrokenExecutor as e:
-    main_logger.error('BrokenExecutor error occurred while running "main.py": %s\n', e, exc_info=True)
-except InvalidStateError as e:
-    main_logger.error('InvalidStateError occurred while running "main.py": %s\n', e, exc_info=True)
-except Exception as e:
-    main_logger.error('An unexpected error occurred while running "main.py": %s\n', e, exc_info=True)
+    if len(sys.argv) != 2:
+        main_logger.error('API name is required as a command-line argument')
+        sys.exit(1)
+
+    api_name = sys.argv[1]
+
+    api_dict_list = read_dict(API_DICT)
+
+    api_url = None
+    for name, url in api_dict_list:
+        if name == api_name:
+            api_url = url
+            break
+
+    if api_url is None:
+        main_logger.error('API name "%s" not found in API_DICT or URL is missing', api_name)
+        sys.exit(1)
+
+    event = Event()
+    queue = Queue(maxsize=3)
+
+    try:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+
+            tasks = [executor.submit(api.get_api_data(api_name, api_url)),
+                     executor.submit(prep.prepare_json_data(queue, event)),
+                     executor.submit(upload.jobs_data_upload_to_db(queue, event))]
+
+        concurrent.futures.wait(tasks)
+    except CancelledError as e:
+        main_logger.error('CancelledError occurred while running "main.py": %s\n', e, exc_info=True)
+    except TimeoutError as e:
+        main_logger.error('TimeoutError occurred while running "main.py": %s\n', e, exc_info=True)
+    except BrokenExecutor as e:
+        main_logger.error('BrokenExecutor error occurred while running "main.py": %s\n', e, exc_info=True)
+    except InvalidStateError as e:
+        main_logger.error('InvalidStateError occurred while running "main.py": %s\n', e, exc_info=True)
+    except Exception as e:
+        main_logger.error('An unexpected error occurred while running "main.py": %s\n', e, exc_info=True)

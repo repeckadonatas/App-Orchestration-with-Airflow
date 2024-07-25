d7c5b671c53ae066fde87c1b5edf75f2172715da
repeckadonatas <repeckadonatas@yahoo.comm>
2024-07-25 22:18:52 +0300

    Container name change

--- scripts/restart_app_airflow_containers.sh
+++ scripts/restart_app_airflow_containers.sh
@@ -2,7 +2,7 @@
 
 echo "Restarting app and airflow Docker containers..."
 docker compose -f docker-compose.app.yaml -p project_container down
-docker compose -f docker-compose.airflow.yaml -p arflow_container down
+docker compose -f docker-compose.airflow.yaml -p airflow_container down
 
 echo "Starting app Docker container..."
 docker compose -f docker-compose.app.yaml -p project_container up -d

1562f02d9e91f5137f2533be5d8005a0f0bc6b95
repeckadonatas <repeckadonatas@yahoo.comm>
2024-07-28 20:49:43 +0300

    Removed unused code. Added 'create_schema' function. Improved 'create_tables' and 'get_tables_in_db' functions

--- src/db_functions/data_movement.py
+++ src/db_functions/data_movement.py
@@ -3,11 +3,13 @@ from datetime import datetime
 import pytz
 import pandas as pd
 from sqlalchemy import inspect
+from sqlalchemy.schema import CreateSchema
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.exc import SQLAlchemyError, OperationalError, DBAPIError, DatabaseError, DisconnectionError
 
 import src.db_functions.db_tables as db_tables
 from src.db_functions.db_connection import db_logger, JobsDataDatabase
+from src.constants import SCHEMAS_IN_DB
 
 
 class DataUpload(JobsDataDatabase):
@@ -17,31 +19,61 @@ class DataUpload(JobsDataDatabase):
 
     def __init__(self):
         super().__init__()
-        super().__enter__()
+
+    def create_schemas(self) -> None:
+        """
+        Creates schemas in a database if they do not exist.
+        """
+        try:
+            inspector = inspect(self.engine)
+            schemas = inspector.get_schema_names()
+
+            for schema_name in SCHEMAS_IN_DB:
+                if schema_name not in schemas:
+                    with self.engine.begin() as connection:
+                        connection.execute(CreateSchema(schema_name))
+                    db_logger.info('Schema "%s" was created successfully!', schema_name)
+                else:
+                    db_logger.info('Schema "%s" already exists. Skipping creation.', schema_name)
+
+        except (OperationalError, DatabaseError, DisconnectionError, DBAPIError) as e:
+            db_logger.error('An error occurred while creating schemas: %s', e, exc_info=True)
 
     def create_tables(self) -> None:
         """
         Creates tables in a database if they do not exist.
         """
         try:
-            for __tablename__ in db_tables.Base.metadata.tables.keys():
-                if not self.engine.dialect.has_table(self.conn, __tablename__):
-                    db_tables.Base.metadata.create_all(self.engine, checkfirst=True)
-                    db_logger.info('Table "%s" created successfully!\n', __tablename__)
-                else:
-                    db_logger.info('Table "%s" already exists. Skipping creation.\n', __tablename__)
-        except (OperationalError, DatabaseError, DisconnectionError, DBAPIError, AttributeError) as e:
-            db_logger.error('An error occurred while creating "%s" table: %s\n', __tablename__, e, exc_info=True)
-            self.conn.rollback()
+            inspector = inspect(self.engine)
 
-    def get_tables_in_db(self) -> list:
+            for table in db_tables.Base.metadata.tables.values():
+                table_name = table.name
+                schema_name = table.schema or 'public'
+
+                if not inspector.has_table(table_name, schema=schema_name):
+                    with self.engine.begin() as connection:
+                        table.create(connection)
+                    db_logger.info('Table "%s" in schema "%s" created successfully!', table_name, schema_name)
+                else:
+                    db_logger.info('Table "%s" in schema "%s" already exists. Skipping creation.',
+                                   table_name, schema_name)
+
+            print()
+        except (OperationalError, DatabaseError, DisconnectionError, DBAPIError, AttributeError) as e:
+            db_logger.error('An error occurred while creating tables: %s', e, exc_info=True)
+
+    def get_tables_in_db(self) -> dict:
         """
         Returns a list of all the tables in the database.
         """
         inspect_db = inspect(self.engine)
-        tables_list = inspect_db.get_table_names()
+        tables_dict = {}
 
-        return tables_list
+        for schema in SCHEMAS_IN_DB:
+            tables_list = inspect_db.get_table_names(schema=schema)
+            tables_dict[schema] = tables_list
+
+        return tables_dict
 
     def load_to_staging(self,
                         json_data: dict,
@@ -69,7 +101,6 @@ class DataUpload(JobsDataDatabase):
                                      if_exists='append',
                                      index=False,
                                      dtype=dtypes)
-            db_logger.info('Loaded data from "%s" to "%s" in staging schema.', api_name, table_name)
 
         except SQLAlchemyError as e:
             db_logger.error("A SQLAlchemy error occurred while loading the data: %s. "
@@ -96,7 +127,7 @@ class DataUpload(JobsDataDatabase):
 
             dataframe = pd.read_sql_query(staging_query,
                                           con=self.engine,
-                                          params=[api_name])
+                                          params={'api_source': api_name})
 
             if not dataframe.empty:
                 return dataframe['data'].iloc[0]
